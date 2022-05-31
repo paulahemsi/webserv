2a42feea85ffbea33102d82e54fdaff18896bd07
Lais Arena <laisarena@gmail.com>
2022-06-01 00:16:24 +0200

    Add pseudocode of a webserver

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,5 +1,22 @@
 
 int main(void)
 {
+	//socket()
+	//bind()
+	//listen()
+
+	while(1)
+	{
+		//accept
+		//while(client writes)
+		//{
+			//Read a request
+			//Execute the request
+			//Write response	
+		//}
+		//close
+	}
+	//close
+
 	return (0);
 }
