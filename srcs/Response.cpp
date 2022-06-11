c2a4fa64759644f7750f4970f088f34a0fbc4df5
Lais Arena <laisarena@gmail.com>
2022-06-11 14:26:20 -0300

    response: fix response

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -6,13 +6,13 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 01:41:10 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/06/08 03:04:00 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/06/11 14:06:28 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Response.hpp"
 
-#define HELLO_WORLD	"<html><body><h1>Hello, World!</h1></body></html>"
+#define HELLO_WORLD	"<html>\n<body>\n<h1>Hello, World!</h1>\n</body>\n</html>\n"
 
 ft::Response::Response(void):	_http_version("HTTP/1.1"),
 								_status_code(200),
@@ -23,9 +23,11 @@ ft::Response::Response(void):	_http_version("HTTP/1.1"),
 	this->_header["Date"] = "Mon, 27 Jul 2009 12:28:53 GMT";
 	this->_header["Server"] = "webserv";
 	this->_header["Last-Modified"] = "Wed, 22 Jul 2009 19:15:56 GMT";
-	this->_header["Content-Length"] = "88";
+	this->_header["Content-Length"] = "53";
 	this->_header["Content-Type"] = "text/html";
-	this->_header["Connection"] = "Closed";
+	//this->_header["Connection"] = "Closed";
+	this->_header["Connection"] = "keep-alive";
+	this->_header["Accept-Ranges"] = "bytes";
 	return ;
 }
 
@@ -41,7 +43,7 @@ std::string ft::Response::_headerToString(void)
 
 	while (it != this->_header.end())
 	{
-		header += it->first + ": " + it->second + '\n';
+		header += it->first + ": " + it->second + CRLF;
 		it++;
 	}
 	return (header);
@@ -58,7 +60,7 @@ std::string ft::Response::toString(void)
 				+ status_code.str() + SP
 				+ this->_reason_phrase + CRLF;
 	header = _headerToString();
-	return (status_line + header + CRLF + CRLF + this->_body );
+	return (status_line + header + CRLF + this->_body );
 }
 
 void	ft::Response::send(int client)
