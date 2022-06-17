96b09dcf2a1af5fb9854e85a7792a6f219dd09b1
paulahemsi <paulahemsi@gmail.com>
2022-06-17 17:35:34 -0300

    parser(location): add parser to location blocks -without actualy saving

--- srcs/Parser.cpp
+++ srcs/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 10:47:31 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/06/17 14:41:21 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/06/17 17:32:13 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ ft::Parser::Parser(void)
 
 ft::Parser::Parser(const char* filename)
 {
-	std::cout << filename << std::endl;
 	this->_file_stream.open(filename, std::ifstream::in);
 	if(!this->_file_stream)
 		throw (OpenFileError());
-	this->_parse_file();
+	_parse_file();
 }
 
 ft::Parser::~Parser(void)
@@ -46,31 +45,147 @@ void ft::Parser::_parse_file(void)
 		std::getline(this->_file_stream, line);
 		if (not_empty(line))
 		{
-			if (line.find(SERVER_CONF_BEGIN, 0) != std::string::npos)
-				this->_parse_server_block(line);
+			if (line == SERVER_BEGIN)
+				_parse_server_block();
 			else
 				throw (ServerConfigurationError());
 		}
 	}
 }
 
-void ft::Parser::_parse_server_block(std::string &line)
+void ft::Parser::_parse_server_block(void)
 {
-	std::cout << line << std::endl;
+	ft::ServerData new_server;
+
 	while (this->_file_stream.good())
 	{
-		std::getline(this->_file_stream, line);
-		if (not_empty(line))
+		std::getline(this->_file_stream, this->_line);
+		if (not_empty(this->_line))
 		{
-			if (line == SERVER_CONF_END)
+			if (this->_line == SERVER_END)
+			{
+				this->_servers.push_back(new_server);
 				return ;
-			std::cout << line << std::endl;
+			}
+			_set_server_conf(new_server);
 		}
 	}
 }
 
-std::vector<ft::Server> ft::Parser::get_servers(void)
+void ft::Parser::_parse_location_block(ft::ServerData &server)
+{
+	ft::LocationData location;
+
+	while (this->_file_stream.good())
+	{
+		std::getline(this->_file_stream, this->_line);
+		if (not_empty(this->_line))
+		{
+			if (this->_line == LOCATION_END)
+			{
+				server.set_location(location);
+				return ;
+			}
+			_set_location_conf(location);
+		}
+	}
+}
+
+void ft::Parser::_set_location_conf(ft::LocationData &location)
+{
+	if (_line_begins_with(ACCEPTED_METHODS))
+		_set_accepted_methods_conf(location);
+	if (_line_begins_with(INDEX))
+		_set_index_conf(location);
+	else if (_line_begins_with(REDIRECTION))
+		_set_redirection_conf(location);
+	else if (_line_begins_with(ROOT))
+		_set_root_conf(location);
+	else if (_line_begins_with(AUTOINDEX))
+		_set_autoindex_conf(location);
+	else if (_line_begins_with(BODY_SIZE))
+		_set_body_size_conf(location);
+}
+
+bool ft::Parser::_line_begins_with(const char *directive)
+{
+	return (this->_line.find(directive, 0) == 0);
+}
+
+void ft::Parser::_set_server_conf(ft::ServerData &server)
+{
+	if (_line_begins_with(LOCATION_BEGIN))
+		_parse_location_block(server);
+	else if (_line_begins_with(LISTEN))
+		_set_listen_conf(server);
+	else if (_line_begins_with(SERVER_NAME))
+		_set_server_name_conf(server);
+	else if (_line_begins_with(ROOT))
+		_set_root_conf(server);
+	else if (_line_begins_with(ERROR_PAGE))
+		_set_error_page_conf(server);
+	else if (_line_begins_with(BODY_SIZE))
+		_set_body_size_conf(server);
+	else
+		throw (ServerConfigurationError());
+}
+
+void ft::Parser::_set_listen_conf(ft::ServerData &server)
+{
+	std::cout << "****************************" << LISTEN << std::endl;
+}
+
+void ft::Parser::_set_server_name_conf(ft::ServerData &server)
+{
+	std::cout << "****************************" << SERVER_NAME << std::endl;
+}
+
+void ft::Parser::_set_root_conf(ft::ServerData &server)
+{
+	std::cout << "****************************" << ROOT << std::endl;
+}
+
+void ft::Parser::_set_error_page_conf(ft::ServerData &server)
+{
+	std::cout << "****************************" << ERROR_PAGE << std::endl;
+}
+
+void ft::Parser::_set_body_size_conf(ft::ServerData &server)
+{
+	std::cout << "****************************" << BODY_SIZE << std::endl;
+}
+
+void ft::Parser::_set_body_size_conf(ft::LocationData &location)
+{
+	std::cout << "-----------------------" << BODY_SIZE << std::endl;
+}
+
+void ft::Parser::_set_root_conf(ft::LocationData &location)
+{
+	std::cout << "-----------------------" << ROOT << std::endl;
+}
+
+void ft::Parser::_set_index_conf(ft::LocationData &location)
+{
+	std::cout << "-----------------------" << INDEX << std::endl;
+}
+
+void ft::Parser::_set_redirection_conf(ft::LocationData &location)
+{
+	std::cout << "-----------------------" << REDIRECTION << std::endl;
+}
+
+void ft::Parser::_set_autoindex_conf(ft::LocationData &location)
+{
+	std::cout << "-----------------------" << AUTOINDEX << std::endl;
+}
+
+void ft::Parser::_set_accepted_methods_conf(ft::LocationData &location)
+{
+	std::cout << "-----------------------" << ACCEPTED_METHODS << std::endl;
+}
+
+std::vector<ft::ServerData> ft::Parser::get_servers(void)
 {
 	return (this->_servers);
 }
-
--- srcs/Parser.hpp
+++ srcs/Parser.hpp
@@ -6,39 +6,70 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 10:47:26 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/06/17 14:41:16 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/06/17 17:31:38 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
+#include <iostream>
 #include <vector>
-#include <fstream>
-#include "Server.hpp"
 #include <string>
+#include <fstream>
+#include "ServerData.hpp"
+#include "LocationData.hpp"
 
-#define SERVER_CONF_BEGIN "server {"
-#define SERVER_CONF_END "};"
+#define SERVER_BEGIN "server {"
+#define SERVER_END "};"
+#define LOCATION_BEGIN "location"
+#define LOCATION_END "}"
+#define LISTEN "listen"
+#define SERVER_NAME "server_name"
+#define ROOT "root"
+#define ERROR_PAGE "error_page"
+#define BODY_SIZE "client_body_size"
+#define ACCEPTED_METHODS "accepted_methods"
+#define INDEX "index"
+#define REDIRECTION "redirection"
+#define AUTOINDEX "autoindex"
 
 namespace ft
 {
 	class Parser
 	{
 		private:
-			std::vector<ft::Server>	_servers;
-			std::ifstream			_file_stream;
-			
-			void					_parse_file(void);
-			void					_parse_server_block(std::string &line);
+			std::vector<ft::ServerData>	_servers;
+			std::ifstream				_file_stream;
+			std::string					_line;
 
+			void	_parse_file(void);
+			void	_parse_server_block(void);
+			void	_parse_location_block(ft::ServerData &server);
+
+			void	_set_server_conf(ft::ServerData &server);
+			void	_set_location_conf(ft::LocationData &location);
+			void	_set_listen_conf(ft::ServerData &server);
+			void	_set_server_name_conf(ft::ServerData &server);
+			void	_set_error_page_conf(ft::ServerData &server);
+			void	_set_root_conf(ft::ServerData &server);
+			void	_set_body_size_conf(ft::ServerData &server);
+
+			void	_set_root_conf(ft::LocationData &location);
+			void	_set_body_size_conf(ft::LocationData &location);
+			void	_set_index_conf(ft::LocationData &location);
+			void	_set_redirection_conf(ft::LocationData &location);
+			void	_set_autoindex_conf(ft::LocationData &location);
+			void	_set_accepted_methods_conf(ft::LocationData &location);
+
+			bool	_line_begins_with(const char *directive);
 			Parser(void);
 
 		public:
 			Parser(const char* filename);
 			~Parser(void);
 			
-			std::vector<ft::Server> get_servers(void);
+			std::vector<ft::ServerData> get_servers(void);
 			
 			class OpenFileError : public std::exception
 			{
