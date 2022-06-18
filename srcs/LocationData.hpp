5e366cb80f46c80e2fbfe85fc2a8aece57fe09ef
paulahemsi <paulahemsi@gmail.com>
2022-06-18 20:46:57 -0300

    locationdata(operator<<): add operator << to LocationData class

--- srcs/LocationData.hpp
+++ srcs/LocationData.hpp
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   LocationData.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 15:30:32 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/06/18 20:12:02 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/06/18 20:40:16 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LOCATIONDATA_HPP
 #define LOCATIONDATA_HPP
 
+#include <iostream>
 #include <string>
 #include <vector>
 #include <set>
@@ -37,13 +38,16 @@ namespace ft
 
 			ft::LocationData	&operator=(ft::LocationData const &right_hand_side);
 		
-			std::set<std::string>		get_accepted_methods(void);
-			std::vector<std::string>	get_index(void);
-			std::string					get_redirection(void);
-			std::string					get_root(void);
-			std::string					get_prefix(void);
-			bool						get_autoindex(void);
-			int							get_body_size(void);
+			std::set<std::string>		get_accepted_methods(void) const ;
+			std::vector<std::string>	get_index(void) const ;
+			std::string					get_redirection(void) const ;
+			std::string					get_root(void) const ;
+			std::string					get_prefix(void) const ;
+			bool						get_autoindex(void) const ;
+			int							get_body_size(void) const ;
+
+			std::string					accepted_methods_to_string(void) const ;
+			std::string					index_to_string(void) const ;
 
 			void	add_accepted_method(std::string new_method);
 			void	add_index(std::string new_index);
@@ -53,6 +57,9 @@ namespace ft
 			void	set_autoindex(bool autoindex_value);
 			void	set_body_size(int size_limit);
 	};
+	
 }
+
+std::ostream &operator<<(std::ostream &outputFile, const ft::LocationData &object);
 
 #endif
