8e118801bcc9e4a34ca5a7e70498bac2308c4168
paulahemsi <paulahemsi@gmail.com>
2022-06-23 19:42:40 -0300

    chore(english): change to english

--- www/supreme/script.js
+++ www/supreme/script.js
@@ -1,194 +1,194 @@
-const tabuleiro = document.querySelector(".tabuleiro");
-const cartas = document.querySelectorAll(".cartas");
+const board = document.querySelector(".board");
+const carts = document.querySelectorAll(".carts");
 const arraySrc = [
-  './imagens/costas.jpg',
-  './imagens/1.jpg',
-  './imagens/2.jpg',
-  './imagens/3.jpg',
-  './imagens/4.jpg',
-  './imagens/5.jpg',
-  './imagens/6.jpg',
-  './imagens/7.jpg',
-  './imagens/8.jpg',
-  './imagens/9.jpg',
-  './imagens/10.jpg',
+  './img/costas.jpg',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
 ];
-const cartaCostas = './imagens/costas.jpg';
-const cartaVazia = './imagens/vazio.jpg';
-let contador = 0;
-let cartasEscolhidas = [];
-let indexCartaEscolhida = [];
-const arrayCartas = [
+const backImg = './img/costas.jpg';
+const emptyImg = './img/vazio.jpg';
+let counter = 0;
+let choosenCarts = [];
+let index = [];
+const arraycarts = [
   {
     id: '1',
-    img: './imagens/1.jpg'
+    img: './img/1.jpg'
   },
   {
     id: '2',
-    img: './imagens/2.jpg'
+    img: './img/2.jpg'
   },
   {
     id: '3',
-    img: './imagens/3.jpg'
+    img: './img/3.jpg'
   },
   {
     id: '4',
-    img: './imagens/4.jpg'
+    img: './img/4.jpg'
   },
   {
     id: '5',
-    img: './imagens/5.jpg'
+    img: './img/5.jpg'
   },
   {
     id: '6',
-    img: './imagens/6.jpg'
+    img: './img/6.jpg'
   },
   {
     id: '7',
-    img: './imagens/7.jpg'
+    img: './img/7.jpg'
   },
   {
     id: '8',
-    img: './imagens/8.jpg'
+    img: './img/8.jpg'
   },
   {
     id: '9',
-    img: './imagens/9.jpg'
+    img: './img/9.jpg'
   },
   {
     id: '10',
-    img: './imagens/10.jpg'
+    img: './img/10.jpg'
   },
   {
     id: '1',
-    img: './imagens/1.jpg'
+    img: './img/1.jpg'
   },
   {
     id: '2',
-    img: './imagens/2.jpg'
+    img: './img/2.jpg'
   },
   {
     id: '3',
-    img: './imagens/3.jpg'
+    img: './img/3.jpg'
   },
   {
     id: '4',
-    img: './imagens/4.jpg'
+    img: './img/4.jpg'
   },
   {
     id: '5',
-    img: './imagens/5.jpg'
+    img: './img/5.jpg'
   },
   {
     id: '6',
-    img: './imagens/6.jpg'
+    img: './img/6.jpg'
   },
   {
     id: '7',
-    img: './imagens/7.jpg'
+    img: './img/7.jpg'
   },
   {
     id: '8',
-    img: './imagens/8.jpg'
+    img: './img/8.jpg'
   },
   {
     id: '9',
-    img: './imagens/9.jpg'
+    img: './img/9.jpg'
   },
   {
     id: '10',
-    img: './imagens/10.jpg'
+    img: './img/10.jpg'
   },
 
 ]
 
 
-function novoJogo() {
-  for (let i = 0; i < cartas.length; i++) {
-    cartas[i].classList.toggle('ativa');
+function newGame() {
+  for (let i = 0; i < carts.length; i++) {
+    carts[i].classList.toggle('ativa');
   }
-  cartasEscolhidas = [];
-  indexCartaEscolhida = [];
-  contador = 0;
-  embaralharCartas(arrayCartas);
-  embaralharTabuleiro();
+  choosenCarts = [];
+  index = [];
+  counter = 0;
+  shuffle(arraycarts);
+  shuffleBoard();
 }
 
-function embaralharCartas(cartas) {
-  let indexAtual = cartas.length, valorTemporario, indexRandom;
+function shuffle(carts) {
+  let indexAtual = carts.length, valorTemporario, indexRandom;
   while (0 !== indexAtual) {
 
     indexRandom = Math.floor(Math.random() * indexAtual);
     indexAtual -= 1;
 
-    valorTemporario = cartas[indexAtual];
-    cartas[indexAtual] = cartas[indexRandom];
-    cartas[indexRandom] = valorTemporario;
+    valorTemporario = carts[indexAtual];
+    carts[indexAtual] = carts[indexRandom];
+    carts[indexRandom] = valorTemporario;
   }
 
-  return cartas;
+  return carts;
 }
 
-function embaralharTabuleiro() {
+function shuffleBoard() {
   const intervalo = setInterval(function () {
-    for (let i = 0; i < cartas.length; i++) {
-      let carta = cartas[i];
+    for (let i = 0; i < carts.length; i++) {
+      let carta = carts[i];
       let index = Math.floor(Math.random() * 10 + 1);
       carta.setAttribute('src', arraySrc[index]);
     }
   }, 30);
   setTimeout(function () {
     clearInterval(intervalo);
-    posicionarCartas();
+    posicionarcarts();
   }, 1750);
 };
 
-function posicionarCartas() {
-  for (let i = 0; i < cartas.length; i++) {
-    let carta = cartas[i];
+function posicionarcarts() {
+  for (let i = 0; i < carts.length; i++) {
+    let carta = carts[i];
     carta.setAttribute('src', arraySrc[0]);
     carta.setAttribute('data-index', i);
-    carta.addEventListener('click', virarCarta);
+    carta.addEventListener('click', turn);
   };
 }
 
 
-function virarCarta() {
-  if ((cartasEscolhidas.length < 2) && (this.classList.contains('ativa'))) {
+function turn() {
+  if ((choosenCarts.length < 2) && (this.classList.contains('ativa'))) {
     let indexCarta = this.getAttribute('data-index');
-    indexCartaEscolhida.push(indexCarta);
-    cartasEscolhidas.push(arrayCartas[indexCarta].id);
-    this.setAttribute('src', arrayCartas[indexCarta].img);
-    if (cartasEscolhidas.length === 2) {
+    index.push(indexCarta);
+    choosenCarts.push(arraycarts[indexCarta].id);
+    this.setAttribute('src', arraycarts[indexCarta].img);
+    if (choosenCarts.length === 2) {
       
-      if (indexCartaEscolhida[0] === indexCartaEscolhida[1]) {
-        indexCartaEscolhida.pop();
-        cartasEscolhidas.pop();
+      if (index[0] === index[1]) {
+        index.pop();
+        choosenCarts.pop();
         return;
       } 
 
-      setTimeout(conferirPar, 500)
+      setTimeout(checkPair, 500)
     }
   }
 }
 
-function conferirPar() {
-  if (cartasEscolhidas[0] === cartasEscolhidas[1]) {
-    for (let i = 0; i < cartasEscolhidas.length; i++) {
-      cartas[indexCartaEscolhida[i]].setAttribute('src', cartaVazia);
-      cartas[indexCartaEscolhida[i]].classList.toggle('ativa');
+function checkPair() {
+  if (choosenCarts[0] === choosenCarts[1]) {
+    for (let i = 0; i < choosenCarts.length; i++) {
+      carts[index[i]].setAttribute('src', emptyImg);
+      carts[index[i]].classList.toggle('ativa');
     }
-    contador++;
-    if (contador === 10) {
-      setTimeout(novoJogo, 500);
+    counter++;
+    if (counter === 10) {
+      setTimeout(newGame, 500);
     }
   } else {
-    cartas[indexCartaEscolhida[0]].setAttribute('src', cartaCostas);
-    cartas[indexCartaEscolhida[1]].setAttribute('src', cartaCostas);
+    carts[index[0]].setAttribute('src', backImg);
+    carts[index[1]].setAttribute('src', backImg);
   }
-  cartasEscolhidas = [];
-  indexCartaEscolhida = [];
+  choosenCarts = [];
+  index = [];
 }
 
 
-document.addEventListener('DOMContentLoaded', novoJogo);
+document.addEventListener('DOMContentLoaded', newGame);
