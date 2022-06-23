9be4683dea75ed13f8a8131803e238dd1d159450
paulahemsi <paulahemsi@gmail.com>
2022-06-23 19:48:38 -0300

    refactor(img): remove duplicate images

--- www/supreme/script.js
+++ www/supreme/script.js
@@ -1,103 +1,103 @@
 const board = document.querySelector(".board");
 const carts = document.querySelectorAll(".carts");
 const arraySrc = [
-  './img/costas.jpg',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg',
+  '../img/costas.jpg',
+  '../img/1.jpg',
+  '../img/2.jpg',
+  '../img/3.jpg',
+  '../img/4.jpg',
+  '../img/5.jpg',
+  '../img/6.jpg',
+  '../img/7.jpg',
+  '../img/8.jpg',
+  '../img/9.jpg',
+  '../img/10.jpg',
 ];
-const backImg = './img/costas.jpg';
-const emptyImg = './img/vazio.jpg';
+const backImg = '../img/costas.jpg';
+const emptyImg = '../img/vazio.jpg';
 let counter = 0;
 let choosenCarts = [];
 let index = [];
 const arraycarts = [
   {
     id: '1',
-    img: './img/1.jpg'
+    img: '../img/1.jpg'
   },
   {
     id: '2',
-    img: './img/2.jpg'
+    img: '../img/2.jpg'
   },
   {
     id: '3',
-    img: './img/3.jpg'
+    img: '../img/3.jpg'
   },
   {
     id: '4',
-    img: './img/4.jpg'
+    img: '../img/4.jpg'
   },
   {
     id: '5',
-    img: './img/5.jpg'
+    img: '../img/5.jpg'
   },
   {
     id: '6',
-    img: './img/6.jpg'
+    img: '../img/6.jpg'
   },
   {
     id: '7',
-    img: './img/7.jpg'
+    img: '../img/7.jpg'
   },
   {
     id: '8',
-    img: './img/8.jpg'
+    img: '../img/8.jpg'
   },
   {
     id: '9',
-    img: './img/9.jpg'
+    img: '../img/9.jpg'
   },
   {
     id: '10',
-    img: './img/10.jpg'
+    img: '../img/10.jpg'
   },
   {
     id: '1',
-    img: './img/1.jpg'
+    img: '../img/1.jpg'
   },
   {
     id: '2',
-    img: './img/2.jpg'
+    img: '../img/2.jpg'
   },
   {
     id: '3',
-    img: './img/3.jpg'
+    img: '../img/3.jpg'
   },
   {
     id: '4',
-    img: './img/4.jpg'
+    img: '../img/4.jpg'
   },
   {
     id: '5',
-    img: './img/5.jpg'
+    img: '../img/5.jpg'
   },
   {
     id: '6',
-    img: './img/6.jpg'
+    img: '../img/6.jpg'
   },
   {
     id: '7',
-    img: './img/7.jpg'
+    img: '../img/7.jpg'
   },
   {
     id: '8',
-    img: './img/8.jpg'
+    img: '../img/8.jpg'
   },
   {
     id: '9',
-    img: './img/9.jpg'
+    img: '../img/9.jpg'
   },
   {
     id: '10',
-    img: './img/10.jpg'
+    img: '../img/10.jpg'
   },
 
 ]
