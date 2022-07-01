ccf5940f231a48a6a8c444e629d71d13bc010a96
paulahemsi <paulahemsi@gmail.com>
2022-07-01 18:35:01 -0300

    style(webserv): add webserv welcome message

--- srcs/WebServer.hpp
+++ srcs/WebServer.hpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 14:01:52 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/06/26 12:00:35 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/07/01 18:32:46 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,30 @@
 #include "RequestProcessor.hpp"
 #include <vector>
 #include <map>
+
+# define BLACK "\033[0;30m"
+# define BOLD_BLACK "\033[1;30m"
+# define RED "\033[0;31m"
+# define BOLD_RED "\033[1;31m"
+# define GREEN "\033[0;32m"
+# define BOLD_GREEN "\033[1;32m"
+# define YELLOW "\033[0;33m"
+# define BOLD_YELLOW "\033[1;33m"
+# define BLUE "\033[0;34m"
+# define BOLD_BLUE "\033[1;34m"
+# define PURPLE "\033[0;35m"
+# define BOLD_PURPLE "\033[1;35m"
+# define CYAN "\033[0;36m"
+# define BOLD_CYAN "\033[1;36m"
+# define WHITE "\033[0;37m"
+# define BOLD_WHITE "\033[1;37m"
+# define RESET_COLOR "\033[0m"
+#define	WEBSERV	"\n\n\n \e[0;38;5;82m\
+			██     ██ ███████ ██████  \e[0;38;5;22m███████ ███████ ██████  ██    ██ \n \e[0;38;5;82m\
+			██     ██ ██      ██   ██ \e[0;38;5;22m██      ██      ██   ██ ██    ██ \n \e[0;38;5;82m\
+			██  █  ██ █████   ██████  \e[0;38;5;22m███████ █████   ██████  ██    ██ \n \e[0;38;5;82m\
+			██ ███ ██ ██      ██   ██  \e[0;38;5;22m    ██ ██      ██   ██  ██  ██  \n \e[0;38;5;82m\
+ 			 ███ ███  ███████ ██████  \e[0;38;5;22m███████ ███████ ██   ██   ████   \n\n\033[0m"
 
 namespace ft
 {
