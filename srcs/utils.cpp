fff919146ec3cada1a7aaeba43c5c97437635243
Lais Arena <laisarena@gmail.com>
2022-07-06 23:54:25 +0200

    fix(st_mtim): change stat struct field to linux version

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 22:06:24 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/07/03 18:16:33 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/07/06 23:52:52 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static std::string http_date(std::time_t *time)
 	std::tm		*gmt_time;
 	char		buff[64];
 
-	std::memset(buff, 0, 64);
+	memset(buff, 0, 64);
 	gmt_time = gmtime(time);
 	strftime(buff, 64, "%a, %d %b %Y %T GMT", gmt_time);
 	return (buff);
@@ -63,5 +63,5 @@ std::string last_modification_time(std::string path)
 	{
 		//lança ServerError?
 	}
-	return (http_date(&s.st_mtimespec.tv_sec));
+	return (http_date(&s.st_mtim.tv_sec));
 }
--- srcs/utils.hpp
+++ srcs/utils.hpp
@@ -6,17 +6,20 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 22:06:54 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/07/03 17:55:38 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/07/06 23:50:24 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include <sys/stat.h>
 #include <iostream>
 #include <sstream>
 #include <ctime>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 bool is_file(std::string path);
 bool is_dir(std::string path);
