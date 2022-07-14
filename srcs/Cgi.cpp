5489f7f12c66599c05659701edcf121109ff67c7
paulahemsi <paulahemsi@gmail.com>
2022-07-13 23:04:19 -0300

    Cgi(parser): add Cgi class

---
+++ srcs/Cgi.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cgi.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 20:52:18 by phemsi-a          #+#    #+#             */
+/*   Updated: 2022/07/13 22:38:21 by phemsi-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Cgi.hpp"
+
+ft::Cgi::Cgi(void)
+{
+	return ;
+}
+
+ft::Cgi::Cgi(ft::Cgi const &other)
+{
+	*this = other;
+}
+
+ft::Cgi::~Cgi(void)
+{
+	return ;
+}
+
+void	ft::Cgi::add_program(std::string extension, std::string path)
+{
+	this->_program[extension] = path;
+}
+
+ft::Cgi &ft::Cgi::operator=(ft::Cgi const &right_hand_side)
+{
+	this->_program = right_hand_side._program;
+	return (*this);
+}
+
+std::string ft::Cgi::get_program(std::string extension) const
+{
+	try
+	{
+		return (this->_program.at(extension));
+	}
+	catch(const std::out_of_range& e)
+	{
+		return ("");
+	}
+}
+
+std::map<std::string, std::string> ft::Cgi::get_program_map(void) const
+{
+	return (this->_program);
+}
+
+std::ostream &operator<<(std::ostream &outputFile, const ft::Cgi &object)
+{
+	std::map<std::string, std::string>					programs;
+	std::map<std::string, std::string>::const_iterator	it;
+	std::map<std::string, std::string>::const_iterator	end;
+
+	programs = object.get_program_map();
+	for (it = programs.begin(); it != programs.end(); it++)
+		outputFile << it->first << " " << it->second << std::endl;
+
+	return (outputFile);
+}
---
+++ srcs/Cgi.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cgi.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 20:42:55 by phemsi-a          #+#    #+#             */
+/*   Updated: 2022/07/13 21:36:36 by phemsi-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CGI_HPP
+#define CGI_HPP
+
+#include <iostream>
+#include <string>
+#include <map>
+
+namespace ft 
+{
+	class Cgi
+	{
+		private:
+			std::map<std::string, std::string>	_program;
+
+
+		public:
+			Cgi(void);
+			Cgi(ft::Cgi const &other);
+			~Cgi(void);
+
+			ft::Cgi	&operator=(ft::Cgi const &right_hand_side);
+
+			std::map<std::string, std::string>	get_program_map(void) const;
+			std::string							get_program(std::string extension) const;
+			void								add_program(std::string extension, std::string path);
+	};
+	
+}
+
+std::ostream &operator<<(std::ostream &outputFile, const ft::Cgi &object);
+
+#endif
